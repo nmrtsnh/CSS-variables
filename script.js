3676294817fd1dda7bec814d1be13ac21e23b79e
Namrata Sinha <namratasinha26@gmail.com>
2021-08-26 11:24:30 +0200

    update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const input = document.querySelector("controls input");
 
 function handleUpdate() {
   const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${suffix}`,
+    this.value + suffix
+  );
 }
 
 input.forEach((input) => input.addEventListener("change", handleUpdate));
